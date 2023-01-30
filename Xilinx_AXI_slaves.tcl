651acfc5166974641581c4b601ad5233d0e292e4
Dan Gastler <dgastler@gmail.com>
2023-01-30 18:12:53 -0500

    Added simple irq interface

--- Xilinx_AXI_slaves.tcl
+++ Xilinx_AXI_slaves.tcl
@@ -713,6 +713,30 @@ proc AXI_IP_IRQ_CTRL {params} {
     puts "Added Xilinx Interrupt Controller AXI Slave: $device_name"
 }
 
+proc AXI_IP_IRQ_SIMPLE {params} {
+    # required values
+    set_required_values $params {device_name irq_dest}
+
+    #to trick Vivado into there being a IRQ_CTRL like thing, we make two not objects
+    create_bd_cell -type ip -vlnv [get_ipdefs -filter {NAME == util_vector_logic}] $device_name
+    set_property -dict [list CONFIG.C_SIZE {1} CONFIG.C_OPERATION {not} CONFIG.LOGO_FILE {data/sym_notgate.png}] [get_bd_cells $device_name]
+    create_bd_cell -type ip -vlnv [get_ipdefs -filter {NAME == util_vector_logic}] ${device_name}_hack
+    set_property -dict [list CONFIG.C_SIZE {1} CONFIG.C_OPERATION {not} CONFIG.LOGO_FILE {data/sym_notgate.png}] [get_bd_cells ${device_name}_hack]
+    #connect up the two nots
+    connect_bd_net [get_bd_pins ${device_name}/Op1] [get_bd_pins ${device_name}_hack/Res]
+    
+
+    #global value for tracking
+    global IRQ_COUNT_${device_name}
+    set IRQ_COUNT_${device_name} 0
+
+    connect_bd_net [get_bd_pins ${device_name}/Res] [get_bd_pins ${irq_dest}]
+    set IRQ_CONCAT ${device_name}_IRQ
+    create_bd_cell -type ip -vlnv  [get_ipdefs -filter {NAME == xlconcat}] ${IRQ_CONCAT}
+    connect_bd_net [get_bd_pins ${IRQ_CONCAT}/dout] [get_bd_pins ${device_name}_hack/Op1]
+    puts "Added Simple Interrupt passthrough $device_name"
+}
+
 proc CONNECT_IRQ {irq_src irq_dest} {
     #connect to global for this irq controller
     global IRQ_COUNT_${irq_dest}
