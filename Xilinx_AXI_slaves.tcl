c26cbe8578f0e308ab8c0118b0c09debf663613d
Dan Gastler <dgastler@gmail.com>
2023-02-02 15:32:04 -0500

    serial nots did not work, but found that concat would work for the irq replacement issue

--- Xilinx_AXI_slaves.tcl
+++ Xilinx_AXI_slaves.tcl
@@ -717,23 +717,19 @@ proc AXI_IP_IRQ_SIMPLE {params} {
     # required values
     set_required_values $params {device_name irq_dest}
 
-    #to trick Vivado into there being a IRQ_CTRL like thing, we make two not objects
-    create_bd_cell -type ip -vlnv [get_ipdefs -filter {NAME == util_vector_logic}] $device_name
-    set_property -dict [list CONFIG.C_SIZE {1} CONFIG.C_OPERATION {not} CONFIG.LOGO_FILE {data/sym_notgate.png}] [get_bd_cells $device_name]
-    create_bd_cell -type ip -vlnv [get_ipdefs -filter {NAME == util_vector_logic}] ${device_name}_hack
-    set_property -dict [list CONFIG.C_SIZE {1} CONFIG.C_OPERATION {not} CONFIG.LOGO_FILE {data/sym_notgate.png}] [get_bd_cells ${device_name}_hack]
-    #connect up the two nots
-    connect_bd_net [get_bd_pins ${device_name}/Op1] [get_bd_pins ${device_name}_hack/Res]
+    #to trick Vivado into there being a IRQ_CTRL like thing
+    create_bd_cell -type ip -vlnv [get_ipdefs -filter {NAME == xlconcat}] $device_name
+    set_property -dict [list CONFIG.NUM_PORTS {1} ] [get_bd_cells $device_name]
     
 
     #global value for tracking
     global IRQ_COUNT_${device_name}
     set IRQ_COUNT_${device_name} 0
 
-    connect_bd_net [get_bd_pins ${device_name}/Res] [get_bd_pins ${irq_dest}]
+    connect_bd_net [get_bd_pins ${device_name}/dout] [get_bd_pins ${irq_dest}]
     set IRQ_CONCAT ${device_name}_IRQ
     create_bd_cell -type ip -vlnv  [get_ipdefs -filter {NAME == xlconcat}] ${IRQ_CONCAT}
-    connect_bd_net [get_bd_pins ${IRQ_CONCAT}/dout] [get_bd_pins ${device_name}_hack/Op1]
+    connect_bd_net [get_bd_pins ${IRQ_CONCAT}/dout] [get_bd_pins ${device_name}/in0]
     puts "Added Simple Interrupt passthrough $device_name"
 }
 
